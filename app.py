f672a4fd5015b5d35973094705084421b9954e3b
researchofs12024-afk <researchofs12024@gmail.com>
2026-04-02 22:43:31 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,9 @@
 import streamlit as st
 import streamlit.components.v1 as components
-import json
 
 st.set_page_config(layout="wide")
 
-# 👉 중구 GeoJSON만 로드
-with open("서울중구.geojson", "r", encoding="utf-8") as f:
-    junggu = json.load(f)
-
-html_code = f"""
+html_code = """
 <!DOCTYPE html>
 <html>
 <head>
@@ -21,22 +16,30 @@ html_code = f"""
 
 <script>
 
-var geojson = {json.dumps(junggu)};
+var geojson = null;
 
-kakao.maps.load(function() {{
+kakao.maps.load(function() {
 
-    var map = new kakao.maps.Map(document.getElementById('map'), {{
-        center: new kakao.maps.LatLng(37.5636, 126.9976), // 👉 중구 중심
+    var map = new kakao.maps.Map(document.getElementById('map'), {
+        center: new kakao.maps.LatLng(37.5636, 126.9976),
         level: 4
-    }});
+    });
+
+    // 🔥 GeoJSON fetch
+    fetch("서울중구.geojson")
+    .then(res => res.json())
+    .then(data => {
+        geojson = data;
+        console.log("GeoJSON loaded");
+    });
 
     var selectedPolygon = null;
 
-    function pointInPolygon(point, vs) {{
+    function pointInPolygon(point, vs) {
         var x = point[0], y = point[1];
         var inside = false;
 
-        for (var i = 0, j = vs.length - 1; i < vs.length; j = i++) {{
+        for (var i = 0, j = vs.length - 1; i < vs.length; j = i++) {
             var xi = vs[i][0], yi = vs[i][1];
             var xj = vs[j][0], yj = vs[j][1];
 
@@ -44,54 +47,59 @@ kakao.maps.load(function() {{
                 && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
 
             if (intersect) inside = !inside;
-        }}
+        }
 
         return inside;
-    }}
+    }
 
-    kakao.maps.event.addListener(map, 'click', function(mouseEvent) {{
+    kakao.maps.event.addListener(map, 'click', function(mouseEvent) {
+
+        if (!geojson) {
+            alert("데이터 로딩 중...");
+            return;
+        }
 
         var lat = mouseEvent.latLng.getLat();
         var lng = mouseEvent.latLng.getLng();
 
-        if (selectedPolygon) {{
+        if (selectedPolygon) {
             selectedPolygon.setMap(null);
-        }}
+        }
 
-        for (var f of geojson.features) {{
+        for (var f of geojson.features) {
 
             var coords = f.geometry.coordinates;
 
-            for (var polygonSet of coords) {{
-                for (var ring of polygonSet) {{
+            for (var polygonSet of coords) {
+                for (var ring of polygonSet) {
 
-                    if (pointInPolygon([lng, lat], ring)) {{
+                    if (pointInPolygon([lng, lat], ring)) {
 
                         var path = ring.map(coord => 
                             new kakao.maps.LatLng(coord[1], coord[0])
                         );
 
-                        selectedPolygon = new kakao.maps.Polygon({{
+                        selectedPolygon = new kakao.maps.Polygon({
                             path: path,
                             strokeWeight: 2,
                             strokeColor: '#FF0000',
                             fillColor: '#FF0000',
                             fillOpacity: 0.3
-                        }});
+                        });
 
                         selectedPolygon.setMap(map);
                         return;
-                    }}
-                }}
-            }}
+                    }
+                }
+            }
 
-        }}
+        }
 
-        console.log("중구 외 지역이거나 필지 없음");
+        console.log("필지 없음");
 
-    }});
+    });
 
-}});
+});
 </script>
 
 </body>
